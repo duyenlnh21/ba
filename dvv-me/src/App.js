b137abf9c4eaca78aaa87ddbfce7f8f50580a6fd
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-14 10:56:58 +0700

    update 1

--- dvv-me/src/App.js
+++ dvv-me/src/App.js
@@ -7,15 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chào mừng quý bạn đến với Dịch vụ vườn - Lê Nguyễn Hữu Duyên
         </p>
+        <h2>ĐI CHỢ</h2>
+        <h2>THĂM QUAN</h2>
+        <h2>THANH TOÁN</h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/groups/1275985819440943"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hỗ trợ: 0375893542
         </a>
       </header>
     </div>
